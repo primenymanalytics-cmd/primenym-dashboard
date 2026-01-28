7c5f34fbb361245696d394d1c4d2e8f00ea3f3a4
Danushka Sandaruwan <sandaruwanhtml@gmail.com>
2026-01-28 21:41:13 +0530

    Add Login, Dashboard, Agency Manager, and Stripe Checkout Second Attempt

--- src/app/api/checkout/route.ts
+++ src/app/api/checkout/route.ts
@@ -2,9 +2,7 @@ import { NextResponse } from "next/server";
 import Stripe from "stripe";
 
 // Initialize Stripe with your Secret Key
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2023-10-16", // Use the latest API version
-});
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, );
 
 export async function POST(req: Request) {
   try {
